07e34232ea3e1e07182a542c05ed2de620fc9e5b
mohamedaminebenmansour <mohamedploy9@gmail.com>
2024-10-26 12:16:20 +0100

    Flutter runApp MaterialApp

--- go_moon/lib/main.dart
+++ go_moon/lib/main.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp();
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'GoMoon ',
+      theme: ThemeData(scaffoldBackgroundColor: Color.fromRGBO(31, 31, 31, 10)),
+      home: Scaffold(),
+    );
+  }
 }
